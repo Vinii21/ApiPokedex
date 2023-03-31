a0a4f8a008669a0c17e0e37853ebef24af6adaf4
Luis Adan Gomez Torres <lgomez1290@gmail.com>
2023-03-30 22:39:58 -0600

    css detalles poke

--- src/pages/DetailPokemon.jsx
+++ src/pages/DetailPokemon.jsx
@@ -10,20 +10,21 @@ const DetailPokemon = () => {
   useEffect(() => {
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-      .then((resp) =>setPokemon(resp.data))
-      .catch((error) => console.error(error))
+      .then((resp) => {
+        setPokemon(resp.data);
+      })
+      .catch((error) => console.error(error));
   }, []);
 
-  const pokeImage = (img) =>{
-    if (img?.sprites?.other?.home?.front_default!=null) {
-      return img?.sprites?.other?.home?.front_default
-    }else if (img?.sprites?.other?.dream_world?.front_default==null){
-      return img?.sprites?.front_default 
-    }else if (img?.sprites?.other?.home?.front_default==null){
-    return  img?.sprites?.other?.dream_world?.front_default
+  const pokeImage = (img) => {
+    if (img?.sprites?.other?.home?.front_default != null) {
+      return img?.sprites?.other?.home?.front_default;
+    } else if (img?.sprites?.other?.dream_world?.front_default == null) {
+      return img?.sprites?.front_default;
+    } else if (img?.sprites?.other?.home?.front_default == null) {
+      return img?.sprites?.other?.dream_world?.front_default;
     }
-  }
-
+  };
 
   return (
     <div className="container-Detailt">
@@ -35,42 +36,51 @@ const DetailPokemon = () => {
             alt={pokemon?.name}
           />
         </div>
-        <div className="container-size">
-          <div className="container-size-secound">
-            <span>Weight {pokemon.weight}</span>
-            <span>Height {pokemon.height}</span>
+        <div className="info-card">
+          <div className="container-size">
+            <div className="container-size-secound">
+              <span>Weight: {pokemon.weight}</span>
+              <span>Height: {pokemon.height}</span>
+            </div>
+            <div className="container-name">
+              <h3>{pokemon?.name}</h3>
+              <p className="id-poki">#{id}</p>
+            </div>
           </div>
-          <div className="container-name">
-            <h3>{pokemon?.name}</h3>
-            <p className="id-poki">#{id}</p>
-          </div>
-        </div>
-        <div className="container-type">
-          <div className="tipe-title">
-            <h3>Type</h3>
-          </div>
-          <div className="container-span-type">
-            <span className="span-type1">{pokemon?.types?.[0]?.type?.name}</span>
-            <span className="span-type2">{pokemon?.types?.[1]?.type?.name}</span>
-          </div>
-        </div>
-        <div className="container-abilities">
-          <div className="abilities-title">
-            <h3>Abilities</h3>
-          </div>
-          <div className="container-span-abilite">
-            <span className="spana-abilities">
-              {pokemon?.abilities?.[0]?.ability?.name}
-            </span>
-            <span className="spana-abilities">
-              {pokemon?.abilities?.[1]?.ability?.name}
-            </span>
+          <div className="type-abilities">
+            <div className="container-type">
+              <div className="tipe-title">
+                <h3>Type</h3>
+              </div>
+              <div className="container-span-type">
+                {pokemon?.types?.map((type, index) => {
+                  return (
+                    <span key={index} className="span-type1">
+                      {type.type.name}
+                    </span>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="container-abilities">
+              <div className="abilities-title">
+                <h3>Abilities</h3>
+              </div>
+              <div className="container-span-abilite">
+                <span className="spana-abilities">
+                  {pokemon?.abilities?.[0]?.ability?.name}
+                </span>
+                <span className="spana-abilities">
+                  {pokemon?.abilities?.[1]?.ability?.name}
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
 
       <div className="container-aside">
-        <h3 >Movements</h3>
+        <h3>Movements</h3>
         {pokemon?.moves?.map((poke) => {
           return (
             <aside className="aside-detailt" key={poke?.move?.url}>
